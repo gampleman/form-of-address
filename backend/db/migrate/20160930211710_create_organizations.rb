ad4b7267ebd38f60c7e72c8881778c14c0d68eef
Jakub Hampl <honitom@seznam.cz>
2016-09-30 21:23:08 +0100

    Add first migration

--- backend/db/migrate/20160930211710_create_organizations.rb
+++ backend/db/migrate/20160930211710_create_organizations.rb
@@ -1,6 +1,23 @@
-        class CreateOrganizations < ActiveRecord::Migration
-          def self.up
-          end
-          def self.down
-          end
-        end
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :address
+      t.timestamps
+    end
+
+    create_table :persons do |t| # let's make life easy with AR
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :address
+
+      t.integer :organization_id
+      t.timestamps
+    end
+
+    add_index :persons, :organization_id
+  end
+end
