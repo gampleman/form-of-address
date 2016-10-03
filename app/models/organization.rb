d6cb66d9f37f60a64ea9fc56ed014b1561e769b3
Jakub Hampl <honitom@seznam.cz>
2016-10-03 20:41:41 +0100

    Fixes bug with organisation creating the wrong thing

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,7 +7,9 @@ class Organization < ActiveRecord::Base
   def people=(person_hashes)
     people_to_add = person_hashes.map do |person_hash|
       if person_hash[:id]
-        Person.find(person_hash[:id]).update_attributes(person_hash.slice(:name, :email, :phone, :address))
+        p = Person.find(person_hash[:id])
+        p.update_attributes(person_hash.slice(:name, :email, :phone, :address))
+        p.id
       else
         Person.create!(person_hash).id
       end
